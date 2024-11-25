3d58604d5c81701ebdcd18a6d7ee7d3e0aa8de60
Bartosz K <kulesza.bartosz@szkola.elektrykplock.edu.pl>
2024-11-25 07:31:07 +0100

    Wykonano Testowanie wiązania danych
    Wykonano Testowanie wiązania danych

---
+++ WiazanieDanychCwiczenia/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WiazanieDanychCwiczenia
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
